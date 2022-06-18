66754bf2df79ad7b5bd39ab68958fe1047efa09e
craig larsen <humansleddog@gmail.com>
2022-06-17 23:38:01 -0500

    update FD dates for 2022
    * update FD dates for 2022
    * update Lat/Long for N0SFF/KC0DMF

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ import logging
 """ name of database file """
 DATABASE_FILENAME = 'n1mm_view.db'
 """ Name of the event/contest """
-EVENT_NAME = 'MyClubCall Field Day'
+EVENT_NAME = 'Field Day'
 """ start time of the event/contest in YYYY-MM-DD hh:mm:ss format """
 # EVENT_START_TIME = datetime.datetime.strptime('2019-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
-EVENT_START_TIME = datetime.datetime.strptime('2020-06-21 18:00:00', '%Y-%m-%d %H:%M:%S')
+EVENT_START_TIME = datetime.datetime.strptime('2022-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
 """ end time of the event/contest """
 # EVENT_END_TIME = datetime.datetime.strptime('2019-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
-EVENT_END_TIME = datetime.datetime.strptime('2020-06-23 17:59:59', '%Y-%m-%d %H:%M:%S')
+EVENT_END_TIME = datetime.datetime.strptime('2022-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
 """ port number used by N1MM+ for UDP broadcasts This matches the port you set in N1MM Configurator UDP logging """
 N1MM_BROADCAST_PORT = 12060
 """ broadcast IP address, used by log re-player. This could be the IP of the N1MM master, or just the last address in the network segment """
@@ -24,9 +24,13 @@ N1MM_BROADCAST_ADDRESS = '192.168.1.255'
 N1MM_LOG_FILE_NAME = 'MyClubCall-2020.s3db'
 """ QTH here is the location of your event. We mark this location with a red dot when we generate the map views."""
 """ QTH Latitude """
-QTH_LATITUDE = 34.0109629
+""" CRAIG: QTH_LATITUDE = 44.857849 """
+""" FRANK: QTH_LATITUDE = 41.849030 """
+QTH_LATITUDE = 41.849030
 """ QTH Longitude """
-QTH_LONGITUDE = -84.4616047
+""" CRAIG: QTH_LONGITUDE = -93.486585 """
+""" FRANK: QTH_LONGITUDE = -93.804940 """
+QTH_LONGITUDE = -93.804940
 """ number of seconds before automatic display change to the next screen """
 DISPLAY_DWELL_TIME = 6
 """ number of seconds before automatic info recalculation from database. Too low makes the Pi work harder. Too high makes a lag in viewing your results. """
